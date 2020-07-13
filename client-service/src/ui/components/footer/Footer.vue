c027b65944b3a15f16972af760126cbeb7c19a96
Maxime HERY <maximehery18@outlook.fr>
2020-07-13 14:19:51 +0200

    feat(client): Migrate to VueJS + implements Pixi.JS

---
+++ client-service/src/ui/components/footer/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer>
+    <div class="footer_container">
+      <div class="left_section">
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/logo.png?raw=true" alt="Return" />
+        </button>
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/rooms.png?raw=true" alt="Rooms" />
+        </button>
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/shop.png?raw=true" alt="Shop" />
+        </button>
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/inventory.png?raw=true" alt="Me" />
+        </button>
+      </div>
+      <div class="middle_section">
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/ghosthead.png?raw=true" alt="Me" />
+        </button>
+        <form>
+          <input type="text" ref="vueref6" maxLength={MAX_CHAT_LENGTH} name="chat" autoComplete="off" placeholder="Click here to chat" />
+          <button>
+            <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/chat_styles.png?raw=true" alt="Chat styles" />
+          </button>
+        </form>
+      </div>
+      <div class="right_section">
+        <button>
+          <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/bottom_bar/all_friends.png?raw=true" alt="Friends" />
+        </button>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import './footer.css'
+
+export default class Footer extends Vue {
+}
+</script>
---
+++ client-service/src/ui/components/header/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <div class="bar_content">
+      <span>{{ this.getMoneyNumber(credits) }}</span>
+      <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/top_bar/credits.png?raw=true" alt="Credits" />
+    </div>
+    <div class="bar_content">
+      <span class="logo">Scuti Project</span>
+    </div>
+    <div class="bar_content">
+      <span>0</span>
+      <img src="https://github.com/Josedn/bobba_client/blob/master/public/images/top_bar/hc.png?raw=true" alt="Habbo club" />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import './header.css'
+
+export default {
+  name: 'Header',
+  props: {
+    credits: {
+      type: Number,
+      default: 900000
+    }
+  },
+  methods: {
+    getMoneyNumber (value: number) {
+      return value.toLocaleString(navigator.language, {
+        minimumFractionDigits: 0
+      })
+    }
+  }
+}
+</script>
